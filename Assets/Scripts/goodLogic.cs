21f8e3b072a075530111194432bb5ccb99279e48
ShlomoZucker <39903252+ShlomoZucker@users.noreply.github.com>
2020-12-20 21:14:07 +0200

    mice

--- Assets/Scripts/goodLogic.cs
+++ Assets/Scripts/goodLogic.cs
@@ -6,19 +6,31 @@ using Random = System.Random;
 public class goodLogic : MonoBehaviour
 {
     private int _angle;
+    private int comeback;
     private Random _random;
     public float mGoodSpeed;
     private void Start()
     {
         _random = new Random();
         _angle = _random.Next(0, 360);
-        float xPosition = Mathf.Cos(_angle);
-        float zPosition = Mathf.Sin(_angle);
+        comeback = 1;
         transform.rotation = Quaternion.Euler(Vector3.forward * _angle);
     }
 
     private void FixedUpdate()
     {
-        transform.position += (transform.up * mGoodSpeed);
+        
+        transform.position += comeback * mGoodSpeed * transform.up;
+    
+        if (Mathf.Abs(transform.position.x) > 3 || Mathf.Abs(transform.position.y) > 3)
+        {
+            comeback = -1;
+        }
+        else
+        {
+            if (Mathf.Abs(transform.position.x) < 0.5 || Mathf.Abs(transform.position.y) < 0.5)           {
+                comeback = 1;
+            }
+        }        
     }
 }
