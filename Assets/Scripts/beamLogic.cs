f367d8325c8ca9cf2a27f6b22f9cc885dc9ac84b
shani shapp <shanishapp@gmail.com>
2020-12-24 15:04:54 +0200

    added shapes

--- Assets/Scripts/beamLogic.cs
+++ Assets/Scripts/beamLogic.cs
@@ -18,29 +18,7 @@ public class beamLogic : MonoBehaviour
         _rb.velocity = transform.up * mBeamSpeed;
     }
 
-    IEnumerator flickerPlayer()
-    {
-        SpriteRenderer sr = GetComponent<SpriteRenderer>();
-        Random random = new Random();
-        int r1 = random.Next(0, 255);
-        int r2 = random.Next(0, 255);
-        int g1 = random.Next(0, 255);
-        int g2 = random.Next(0, 255);
-        int b1 = random.Next(0, 255);
-        int b2 = random.Next(0, 255);
-        Color tmp = sr.color;
-        while(true)
-        {
-            sr.color = new Color(tmp.r,tmp.g, tmp.b,0);
-            yield return new WaitForSeconds (0.1f);
-            sr.color = new Color(r1,g1, b1,255);
-            yield return new WaitForSeconds (0.1f); 
-            sr.color = new Color(r2,b2, g2,0);
-            yield return new WaitForSeconds (0.1f);
-            sr.color = new Color(r1,g1, b1,255);
-            yield return new WaitForSeconds (0.1f);
-        }
-    }
+   
 
    
 }
---
+++ Assets/Scripts/shapeLogic.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = System.Random;
+
+public class shapeLogic : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(flickerPlayer());
+    }
+
+    IEnumerator flickerPlayer()
+    {
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        System.Random random = new Random();
+        float r1 = (float) random.NextDouble();
+        float r2 = (float) random.NextDouble();
+        float g1 = (float) random.NextDouble();
+        float g2 = (float) random.NextDouble();
+        float b1 = (float) random.NextDouble();
+        float b2 = (float) random.NextDouble();
+        Color tmp = sr.color;
+        while(true)
+        {
+            sr.color = new Color(tmp.r,tmp.g, tmp.b,0);
+            yield return new WaitForSeconds (0.1f);
+            sr.color = new Color(r1,g1, b1,1f);
+            yield return new WaitForSeconds (1f); 
+            sr.color = new Color(r2,b2, g2,0);
+            yield return new WaitForSeconds (0.1f);
+            sr.color = new Color(r1,g1, b1,1f);
+            yield return new WaitForSeconds (1f);
+        }
+    }
+
+}
