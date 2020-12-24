cc22d2965f12a654f1f79526130084c1c335e6ff
ShlomoZucker <39903252+ShlomoZucker@users.noreply.github.com>
2020-12-24 17:27:31 +0200

    Enemy prefab

---
+++ Assets/EnemyScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+
+    public static int speed = 1;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float step =  speed * Time.deltaTime;
+        transform.position = Vector2.MoveTowards(transform.position, new Vector2(0.0f, 0.0f), step);
+    }
+}
